db04dcfb8cef16104906afda23395705a7daaa63
Harrison Dong <harrison@bubblelab.com>
2017-11-22 14:19:57 +0900

    Fix getRandomColor so it returns colorRGBA

--- src/rviz_tools_py/rviz_tools.py
+++ src/rviz_tools_py/rviz_tools.py
@@ -62,6 +62,8 @@ class RvizMarkers(object):
         # Create the Rviz Marker Publisher
         self.loadMarkerPublisher(wait_time)
 
+        # Make a list of the color names to choose from
+        self.all_colors = ['red', 'green', 'blue', 'grey', 'white', 'orange', 'yellow', 'brown', 'pink', 'lime_green', 'purple']
 
     def setDefaultMarkerParams(self):
         """
@@ -409,26 +411,11 @@ class RvizMarkers(object):
 
         @return color (ColorRGBA)
         """
-
-        # Make a list of the color names to choose from
-        all_colors = []
-        all_colors.append('red')
-        all_colors.append('green')
-        all_colors.append('blue')
-        all_colors.append('grey')
-        all_colors.append('white')
-        all_colors.append('orange')
-        all_colors.append('yellow')
-        all_colors.append('brown')
-        all_colors.append('pink')
-        all_colors.append('lime_green')
-        all_colors.append('purple')
-
         # Chose a random color name
-        rand_num =  random.randint(0, len(all_colors) - 1)
-        rand_color_name = all_colors[rand_num]
+        rand_num =  random.randint(0, len(self.all_colors) - 1)
+        rand_color_name = self.all_colors[rand_num]
 
-        return rand_color_name
+        return self.getColor(rand_color_name)
 
 
     def publishSphere(self, pose, color, scale, lifetime=None):
@@ -1382,3 +1369,4 @@ def mat_to_pose(mat):
     pose.orientation.w = quat[3]
 
     return pose
+
